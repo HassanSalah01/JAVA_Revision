b153034261908c74a2b55bde450c339336563e40
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-06 12:46:17 +0200

    Added_CompanyEmp

---
+++ CompanyEmp/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Employee e1 = new Employee("sara", "Marketing");
+        System.out.println(e1.getId());
+    }
+}
---
+++ CompanyEmp/src/Employee.java
@@ -0,0 +1,43 @@
+public class Employee {
+    static int empId = 0;
+    private String name ;
+    private int id ;
+    private String department;
+
+
+    public Employee(String name , String department){
+        Employee.empId++;
+        this.id = Employee.empId;
+        this.name = name ; 
+        this.department = department;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+
+
+    
+}
