5e50522626141b0a9df6e1ae53bf20ef1afe5a3f
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-08 17:35:39 +0200

    Added_GUI_Class

--- CompanyEmp/src/App.java
+++ CompanyEmp/src/App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        FullTimeEmployee e1 = new FullTimeEmployee("sara","Marketing",'A');
-        System.out.println(e1.printEmployeeData());
-        PartTimeEmployee p1 = new PartTimeEmployee("ahmed","Finance");
-        System.out.println(p1.printEmployeeData());
+        // FullTimeEmployee e1 = new FullTimeEmployee("sara","Marketing",'A');
+        // System.out.println(e1.printEmployeeData());
+        // PartTimeEmployee p1 = new PartTimeEmployee("ahmed","Finance");
+        // System.out.println(p1.printEmployeeData());
+
+        GUI.guiInterface();
     }
 }
---
+++ CompanyEmp/src/GUI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GUI {
+
+    public static int guiInterface(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome To Company");
+        System.out.println("If You Are Current Employee Please Press 1");
+        System.out.println("If You Are A New Employee Please Press 2 ");
+        int choice = scan.nextInt();
+        scan.close();
+        return choice;
+    }
+    
+}
