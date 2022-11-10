e38c3fe3ef3ad5e856732d32b220a7b2fc022dfa
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-10 20:31:29 +0200

    Updated_WriteBuffer

--- ReadAndWrite/src/App.java
+++ ReadAndWrite/src/App.java
@@ -3,9 +3,12 @@ import java.io.FileWriter;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        String[] sub = {"Math","Science","English","french"};
         FileWriter f1 = new FileWriter("text.txt");
         BufferedWriter wri = new BufferedWriter(f1);
-        wri.write("Hello WOrld !");
+        for (String s : sub){
+            wri.write(s+"\n");
+        }
         wri.close();
     }
 }
